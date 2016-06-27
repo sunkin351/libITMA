eb45eb2a90830baa16dd87d1219043e40c4ea79a
sunkin351 <17joshuanewcomb@gmail.com>
2016-06-27 15:05:07 -0700

    Add files via upload

---
+++ ITMA.cpp
@@ -0,0 +1,463 @@
+#include "ITMA.hpp"
+
+
+namespace MTMA
+{
+	inline void lock(std::mutex & mtx)
+	{
+		while (!mtx.try_lock())
+			std::this_thread::sleep_for(std::chrono::milliseconds(2));
+	}
+
+	inline void unlock(std::mutex & mtx)
+	{
+		mtx.unlock();
+	}
+
+
+	MContext::MContext()
+	{
+		context = std::thread(std::bind(&MContext::ThreadStart, this)); //Start Message Manager on context creation.
+		context.detach();
+	}
+
+	std::shared_ptr<pipe> MContext::CreatePipe(int chan)
+	{
+		lock(mtx);
+		int ret = pipes.size();
+		pipes.push_back(std::make_shared<pipe>(pipe(chan)));
+		unlock(mtx);
+		return pipes[ret];
+	}
+
+	void MContext::DestroyPipe(std::shared_ptr<pipe> & pip)
+	{
+		int i = 0;
+		lock(mtx);
+		for (auto it : pipes)
+		{
+			if (it == pip)
+			{
+				pipes.erase(pipes.begin() + i);
+				pip.reset();
+				break;
+			}
+			i++;
+		}
+		unlock(mtx);
+	}
+
+	void MContext::ThreadStart()
+	{
+		while (true)
+		{
+			lock(mtx);
+			if (pipes.size() != 0) //Check to see if there are pipes to process.
+			{
+				for (int a = 0; a != pipes.size(); ++a) //Primary loop looking in all pipes for messages that need moving.
+				{
+					if (!pipes[a]->out.empty()) //Check to see if there are messages to move.
+					{
+						Message msg; //buffer message
+						pipes[a]->ctxpop(msg);
+						int channel = pipes[a]->channel;
+
+						for (int b = 0; b != pipes.size(); ++b) // Secondary loop to check all pipes for certain criteria such as channel and subscription
+						{
+							if (a != b) //Skips sender pipe
+							{
+								if (pipes[b]->channel == channel)
+								{
+									if (msg.signature == "") // If message signature is blank, message will be given to everyone on the same channel as the sender
+									{
+										pipes[b]->ctxpush(msg);
+										continue;
+									}
+
+									if (pipes[b]->subscription.size() == 0) // if there are no subscriptions on a pipe and the message DOES have a signature, then said pipe will be skipped.
+									{
+										continue;
+									}
+
+									// Else if a pipe does have subscriptions,
+									// they will be stepped through and compared to the messages signature.
+									// If there is a match, the message will be added to the pipe, and the loop will 
+									// exit prematurely.
+									for (int c = 0; c != pipes[b]->subscription.size(); ++c)
+									{
+										if (pipes[b]->subscription[c] == msg.signature)
+										{
+											pipes[b]->ctxpush(msg);
+											break;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			unlock(mtx);
+			std::this_thread::sleep_for(std::chrono::milliseconds(1)); //To keep CPU usage down while still being responsive.
+		}
+	}
+
+
+	Channel::Channel(MContext & ctx) : _ctx(ctx)
+	{
+	}
+
+	Channel::Channel(MContext & ctx, int channel) : _ctx(ctx)
+	{
+		open(channel);
+	}
+
+
+	Channel::~Channel()
+	{
+		close();
+	}
+
+	void Channel::open(int channelNumber)
+	{
+		//A channel object can only have one pipe at a time.
+		if (pip)
+			_ctx.DestroyPipe(pip);
+
+		pip = _ctx.CreatePipe(channelNumber);
+	}
+
+	void Channel::close()
+	{
+		if (pip)
+		{
+			while (!pip->out.empty())
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(5));
+			}
+			_ctx.DestroyPipe(pip);
+		}
+	}
+
+	bool Channel::poll()
+	{
+		return !pip->in.empty();
+	}
+
+	void Channel::subscribe(std::string sub)
+	{
+		pip->subscription.push_back(sub);
+	}
+
+	void Channel::unsubscribe(std::string sub)
+	{
+		for (auto it = pip->subscription.begin(); it != pip->subscription.end(); ++it)
+		{
+			if (*it == sub)
+			{
+				pip->subscription.erase(it);
+				break;
+			}
+		}
+	}
+
+
+	pipe::pipe(int _channel)
+	{
+		channel = _channel;
+	}
+
+	pipe::pipe(const pipe & tocopy)
+	{
+		channel = tocopy.channel;
+		subscription = tocopy.subscription;
+	}
+
+	pipe::~pipe()
+	{
+		while (!in.empty())
+			in.pop();
+		while (!out.empty())
+			out.pop();
+
+		subscription.clear();
+	}
+
+	void pipe::send(std::string dat, std::string sig, bool more)
+	{
+		std::string * data = new std::string(dat);
+		send((void*)data, string, sig, more);
+	}
+
+	void pipe::send(bool data, std::string sig, bool more)
+	{
+		bool * dat = new bool(data);
+		send((void*)dat, _bool, sig, more);
+	}
+
+	void pipe::send(char data, std::string sig, bool more)
+	{
+		char* dat = new char(data);
+		send((void*)dat, _8bitS, sig, more);
+	}
+
+	void pipe::send(short data, std::string sig, bool more)
+	{
+		short* dat = new short(data);
+		send((void*)dat, _16bitS, sig, more);
+	}
+
+	void pipe::send(int data, std::string sig, bool more)
+	{
+		int * dat = new int(data);
+		send((void*)dat, _32bitS, sig, more);
+	}
+
+	void pipe::send(long data, std::string sig, bool more)
+	{
+		long * dat = new long(data);
+		send((void*)dat, _64bitS, sig, more);
+	}
+
+	void pipe::send(unsigned char data, std::string sig, bool more)
+	{
+		unsigned char* dat = new unsigned char(data);
+		send((void*)dat, _8bitU, sig, more);
+	}
+
+	void pipe::send(unsigned short data, std::string sig, bool more)
+	{
+		unsigned short* dat = new unsigned short(data);
+		send((void*)dat, _16bitU, sig, more);
+	}
+
+	void pipe::send(unsigned int data, std::string sig, bool more)
+	{
+		unsigned int* dat = new unsigned int(data);
+		send((void*)dat, _32bitU, sig, more);
+	}
+
+	void pipe::send(unsigned long data, std::string sig, bool more)
+	{
+		unsigned long* dat = new unsigned long(data);
+		send((void*)dat, _64bitU, sig, more);
+	}
+
+	void pipe::send(void * data, std::string signature, bool more)
+	{
+		send(data, _voidptr, signature, more);
+	}
+
+	bool pipe::recieve(std::string & str)
+	{
+		Message msg;
+		bool rc = recieve(msg);
+		if (rc)
+		{
+			if (msg.type != string)
+			{
+				throw std::exception("Message data not of type string.");
+			}
+			str = *static_cast<std::string*>(msg.data.get());
+		}
+		return rc;
+	}
+
+	bool pipe::recieve(bool & data)
+	{
+		Message msg;
+		bool rc = recieve(msg);
+		if (rc) {
+			if (msg.type != _bool)
+			{
+				throw std::exception("Message data not of type bool.");
+			}
+			data = *static_cast<bool*>(msg.data.get());
+		}
+		return rc;
+	}
+
+	bool pipe::recieve(char & dest)
+	{
+		Message msg;
+		bool rc = recieve(msg);
+		if (rc) {
+			if (msg.type != _8bitS)
+			{
+				throw std::exception("Message data not of type char.");
+			}
+			dest = *static_cast<char*>(msg.data.get());
+		}
+		return rc;
+	}
+
+	bool pipe::recieve(short & dest)
+	{
+		Message msg;
+		bool rc = recieve(msg);
+		if (rc) {
+			if (msg.type != _16bitS)
+			{
+				throw std::exception("Message data not of type short.");
+			}
+			dest = *static_cast<short*>(msg.data.get());
+		}
+		return rc;
+	}
+
+	bool pipe::recieve(int & dest)
+	{
+		Message msg;
+		bool rc = recieve(msg);
+		if (rc) {
+			if (msg.type != _32bitS)
+			{
+				throw std::exception("Message data not of type int.");
+			}
+			dest = *static_cast<int*>(msg.data.get());
+		}
+		return rc;
+	}
+
+	bool pipe::recieve(long & dest)
+	{
+		Message msg;
+		bool rc = recieve(msg);
+		if (rc) {
+			if (msg.type != _64bitS)
+			{
+				throw std::exception("Message data not of type long.");
+			}
+			dest = *static_cast<long*>(msg.data.get());
+		}
+		return rc;
+	}
+
+	bool pipe::recieve(unsigned char & dest)
+	{
+		Message msg;
+		bool rc = recieve(msg);
+		if (rc) {
+			if (msg.type != _8bitU)
+			{
+				throw std::exception("Message data not of type unsigned char.");
+			}
+			dest = *static_cast<unsigned char*>(msg.data.get());
+		}
+		return rc;
+	}
+
+	bool pipe::recieve(unsigned short & dest)
+	{
+		Message msg;
+		bool rc = recieve(msg);
+		if (rc) {
+			if (msg.type != _16bitU)
+			{
+				throw std::exception("Message data not of type unsigned short.");
+			}
+			dest = *static_cast<unsigned short*>(msg.data.get());
+		}
+		return rc;
+	}
+
+	bool pipe::recieve(unsigned int & dest)
+	{
+		Message msg;
+		bool rc = recieve(msg);
+		if (rc) {
+			if (msg.type != _32bitU)
+			{
+				throw std::exception("Message data not of type unsigned int.");
+			}
+			dest = *static_cast<unsigned int*>(msg.data.get());
+		}
+		return rc;
+	}
+
+	bool pipe::recieve(unsigned long & dest)
+	{
+		Message msg;
+		bool rc = recieve(msg);
+		if (rc) {
+			if (msg.type != _64bitU)
+			{
+				throw std::exception("Message data not of type unsigned long.");
+			}
+			dest = *static_cast<unsigned long*>(msg.data.get());
+		}
+		return rc;
+	}
+
+	void * pipe::recieve()
+	{
+		Message msg;
+		bool rc = recieve(msg);
+		if (rc) {
+			if (msg.type != _voidptr)
+			{
+				throw std::exception("Message data not of type unsigned long.");
+			}
+			return msg.data.get();
+		}
+		else {
+			return nullptr;
+		}
+	}
+
+	void pipe::send(void * data, char type, std::string sig, bool more, int size)
+	{
+		Message buffer;
+		buffer.data.reset(data);
+		buffer.type = type;
+		buffer.signature = sig;
+		buffer.more = more;
+		buffer.size = size;
+
+		lock();
+		out.push(buffer);
+		unlock();
+	}
+
+	bool pipe::recieve(Message & msg)
+	{
+		if (!in.empty())
+		{
+			lock();
+			msg = in.front();
+			in.pop();
+			unlock();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	void pipe::lock()
+	{
+		::lock(mtx);
+	}
+
+	void pipe::unlock()
+	{
+		::unlock(mtx);
+	}
+
+	void pipe::ctxpush(Message & msg)
+	{
+		lock();
+		in.push(msg);
+		unlock();
+	}
+
+	void pipe::ctxpop(Message & msg)
+	{
+		lock();
+		msg = out.front();
+		out.pop();
+		unlock();
+	}
+
+}
---
+++ ITMA.hpp
@@ -0,0 +1,230 @@
+/*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
+
+#pragma once
+
+#ifndef MTMA_HPP
+#define MTMA_HPP
+
+#include <thread>
+#include <string>
+#include <deque>
+#include <queue>
+#include <memory>
+#include <mutex>
+
+//Windows dll EXPORT definition
+//TODO: add Cross-platform dynamic library macros
+#ifdef MTMA_EXPORT
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT __declspec(dllimport)
+#endif //MTMA_EXPORT
+
+namespace MTMA
+{
+	class pipe;
+
+	class EXPORT MContext
+	{
+		std::thread context;
+		std::deque<std::shared_ptr<pipe>> pipes;
+		std::mutex mtx;
+	public:
+		MContext();
+		std::shared_ptr<pipe> CreatePipe(int chan);
+		void DestroyPipe(std::shared_ptr<pipe> & pip);
+
+	private:
+		void ThreadStart();
+	};
+
+	//ZMQPP socket style setup
+	class EXPORT Channel
+	{
+		std::shared_ptr<pipe> pip;
+		MContext & _ctx;
+	public:
+		Channel(MContext& ctx);
+		Channel(MContext & ctx, int ChannelNumber);
+		~Channel();
+
+		void open(int channelNumber);
+		void close();
+
+		template<typename T>
+		void send(T & object, std::string signature = "", bool more = false)
+		{
+			pip->send(object, signature, more);
+		}
+
+		template<typename T>
+		void recieve(T & object)
+		{
+			while (pip->in.empty())
+				std::this_thread::sleep_for(std::chrono::milliseconds(2)); //blocking read
+			pip->recieve(object);
+		}
+
+		bool poll();
+
+		void subscribe(std::string sub);
+		void unsubscribe(std::string sub);
+	};
+
+	struct Message
+	{
+		std::string signature;
+		std::shared_ptr<void> data;
+		char type;
+		bool more;
+		int size;
+
+		Message() {}
+
+		Message(Message& msg)
+		{
+			copy(msg);
+		}
+
+		Message(const Message& msg)
+		{
+			copy(const_cast<Message&>(msg));
+		}
+
+		Message & operator=(Message&& source)
+		{
+			copy(source);
+			return *this;
+		}
+		Message & operator=(Message& source)
+		{
+			copy(source);
+			return *this;
+		}
+
+		void copy(Message& source)
+		{
+			signature = source.signature;
+			data = source.data;
+			type = source.type;
+			more = source.more;
+			size = source.size;
+		}
+	};
+
+	class EXPORT pipe
+	{
+		std::queue<Message> in; //incoming messages
+		std::queue<Message> out; //outgoing messages
+		std::vector<std::string> subscription; //list of subscriptions for a pipe
+		int channel; //channel for a pipe
+		std::mutex mtx; //Mutex for synchronous access to the queues.
+		enum //Message type specifiers
+		{
+			string,
+			_bool,
+			_8bitS,
+			_16bitS,
+			_32bitS,
+			_64bitS,
+			_8bitU,
+			_16bitU,
+			_32bitU,
+			_64bitU,
+			_voidptr,
+			CustomObject
+		};
+	public:
+		//constructors
+		pipe(int _channel = 0);
+		pipe(const pipe& tocopy);
+
+		//destructor
+		~pipe();
+
+		//Methods that convert to void* data
+		void send(std::string data, std::string signature, bool more = false);
+		void send(bool data, std::string signature, bool more = false);
+
+		void send(char data, std::string signature, bool more = false);
+		void send(short data, std::string signature, bool more = false);
+		void send(int data, std::string signature, bool more = false);
+		void send(long data, std::string signature, bool more = false);
+
+		void send(unsigned char data, std::string signature, bool more = false);
+		void send(unsigned short data, std::string signature, bool more = false);
+		void send(unsigned int data, std::string signature, bool more = false);
+		void send(unsigned long data, std::string signature, bool more = false);
+
+
+		template<class T, int size = sizeof(T)>
+		void send(T & object, std::string signature, bool more = false)
+		{
+			T* pointer = new T(object);
+			send((void*)pointer, CustomObject, signature, more, size);
+		}
+
+		//Methods that convert from void* data
+		bool recieve(std::string & dest);
+		bool recieve(bool& dest);
+
+		bool recieve(char& dest);
+		bool recieve(short& dest);
+		bool recieve(int& dest);
+		bool recieve(long& dest);
+
+		bool recieve(unsigned char& dest);
+		bool recieve(unsigned short& dest);
+		bool recieve(unsigned int& dest);
+		bool recieve(unsigned long& dest);
+
+		//void* message feature. Dont use unless you know what your doing.
+		void send(void* data, std::string signature, bool more = false);
+		void* recieve(); //returns a void* instead of a bool, but throws if the original message was not a void*.
+
+		template<class T, int size = sizeof(T)>
+		bool recieve(T & dest)
+		{
+			Message msg;
+			bool rc = recieve(msg);
+			if (rc) {
+				if (msg.type != CustomObject || size != msg.size)
+				{
+					throw std::exception("Message data not of the same data type.");
+				}
+				dest = *static_cast<T*>(msg.data.get());
+			}
+			return rc;
+		}
+
+	private:
+		//Methods that send/recieve messages
+		inline void send(void * data, char type, std::string signature, bool more = false, int size = 0);
+		inline bool recieve(Message & msg);
+
+		//Methods that control the mutex
+		void lock();
+		void unlock();
+
+		//Methods that are used in the Message Managing context
+		void ctxpush(Message & msg);
+		void ctxpop(Message & msg);
+
+		friend class MContext;
+		friend class Channel;
+	};
+
+	
+}//namespace MTMA
+
+#endif //MTMA_HPP
